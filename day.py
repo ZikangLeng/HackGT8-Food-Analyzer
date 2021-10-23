2bd6468be5a632f7ce589d44c75e6f9128c35600
Liwei Sun <liweisun16@.gmail.com>
2021-10-23 18:23:15 -0400

    day fixed

--- day.py
+++ day.py
@@ -1,13 +1,18 @@
-class Day:
+
+class Day():
     totalCal=0
     totalProtein = 0
     totalFat = 0
     totalCarbs = 0
     foods = []
 
-    def add(name, cal, protein, fat, carbs,self, foods):
-        self.foods.append(name)
+    def add(self, foodname, cal, protein, fat, carbs):
+        self.foods.append(foodname)
+        self.totalCal += cal
+        self.totalProtein += protein
+        self.totalFat += fat
+        self.totalCarbs += carbs
+    
+    def getDayFacts(self):
+        return totalCal + 
 
-
-test = Day()    
-test.add("bruh", 1,2,3,4)
