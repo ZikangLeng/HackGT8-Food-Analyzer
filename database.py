3ea381dd0888f11d3a94d2fa960fff9bf2aeb75d
Liwei Sun <liweisun16@.gmail.com>
2021-10-23 14:41:21 -0400

    fixed

--- database.py
+++ database.py
@@ -10,30 +10,31 @@ class database:
             if name.__contains__(keyword):
                 return id
 
-    def getNutrition(id):
+    def getNutrition(id, mass):
         output = []
         NutritionFile = csv.reader(open("food_nutrient.csv","r"), delimiter= ',')
         NutritionIDFile = csv.reader(open("nutrient.csv", "r"), delimiter= ',')
         for row in NutritionFile:
-            if row[1] is id:
-                if row[2] is '1003':
-                    output.append("Protein: " + row[3] + " G")
-                elif row[2] is '1004':
-                    output.append("Fat: " + row[3] + " G")
-                elif row[2] is '1005':
-                    output.append("Carbs: " + row[3] + " G")
-        outputStr = str
+            if row[1] == id:
+                if row[2] == '1003':
+                    output.append("Protein: " + str(float(row[3])*mass/100) + " g")
+                elif row[2] == '1004':
+                    output.append("Fat: " + str(float(row[3])*mass/100) + " g")
+                elif row[2] == '1005':
+                    output.append("Carbs: " + str(float(row[3])*mass/100) + " g")
+        
+        outputStr = ""
         for nutrient in output:
-            outputStr = outputStr + nutrient + "/n"
-            return outputStr 
+            outputStr = outputStr + str(nutrient) + "\n"
+        return outputStr 
                 
     def getCal(id):
         NutritionFile = csv.reader(open("food_nutrient.csv","r"), delimiter= ',')
         for row in NutritionFile:
-            if row[1] is id and row[2] is "1008":
+            if row[1] == id and row[2] == "1008":
                 return row[2]
                 
-print(database.getNutrition(database.getID("Milk")))
+print(database.getNutrition(database.getID("Milk"), 300))
 
 
 
