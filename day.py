782fed1e27914019042a5d8742806a7d079f5976
ZikangLeng <90108902+ZikangLeng@users.noreply.github.com>
2021-10-23 23:41:32 -0400

    fixed

--- day.py
+++ day.py
@@ -1,16 +1,24 @@
+from food import *
 class Day():
     totalCal=0
     totalProtein = 0
     totalFat = 0
     totalCarbs = 0
     foods = []
-
-    def add(self, foodname, cal, protein, fat, carbs):
-        self.foods.append(foodname)
-        self.totalCal += cal
-        self.totalProtein += protein
-        self.totalFat += fat
-        self.totalCarbs += carbs
-    
     def getDayFacts(self):
         return "Total Calories: " + str(self.totalCal)  + " kCal Total Protein: " + str(self.totalProtein)  + " g Total Fat: " + str(self.totalFat) + " g Total Carbohydrates: " + str(self.totalCarbs) + " g"
+
+    def add(self, foodname):
+        self.foods.append(foodname)
+        self.totalCal += foodname.cal
+        self.totalProtein += foodname.prot
+        self.totalFat += foodname.fat
+        self.totalCarbs += foodname.carb
+
+
+    
+    
+testFood = Food("milk", 200)
+test = Day()
+test.add(testFood)
+print(test.getDayFacts())
